2956e057335f02a87a99b771fdebd975307f8300
dark011caique <caique.quiky@gmail.com>
2024-10-14 14:04:54 -0300

    inicio de intregracao continua

---
+++ database/db.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"github.com/guilhermeonrails/api-go-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaComBancoDeDados() {
+	endereco := os.Getenv("DB_HOST")
+	usuario := os.Getenv("DB_USER")
+	senha := os.Getenv("DB_PASSWORD")
+	nomeBanco := os.Getenv("DB_NAME")
+	portaBanco := os.Getenv("DB_PORT")
+
+	// Corrigindo a string de conexão
+	stringDeConexao := "host=" + endereco + " user=" + usuario + " password=" + senha + " dbname=" + nomeBanco + " port=" + portaBanco + " sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados: ", err)
+	}
+
+	_ = DB.AutoMigrate(&models.Aluno{})
+}
